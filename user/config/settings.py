acac063adf134c0865159613cb5be497ce88c0e6
merveipcioglu <merveipcioglu@gmail.com>
2025-03-04 12:34:57 +0300

    uodate database name

--- user/config/settings.py
+++ user/config/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nextTest',
+        'NAME': 'next',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
